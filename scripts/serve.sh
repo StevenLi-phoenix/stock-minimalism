5034ff35359d65c0bae165ef4d2fcd9c82ec6203
Steven Li <lishuyustevenli@163.com>
2026-09-12 15:38:19 -0400

    fix build script with not found

--- scripts/serve.sh
+++ scripts/serve.sh
@@ -6,10 +6,10 @@ set -euo pipefail
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 PORT="${1:-8000}"
 
-cd "${ROOT_DIR}/build/web"
-
 # force rebuild everytime
 bash "${ROOT_DIR}/scripts/build_web.sh"
+
+cd "${ROOT_DIR}/build/web"
 
 echo "Serving ${ROOT_DIR}/build/web at http://localhost:${PORT}/index.html"
 python3 -m http.server "${PORT}"
